2c144a44fd7d0ff15a40e7d8e22ec3870c408f98
Pratik <pratik080.rejoice@gmail.com>
2026-06-26 17:07:32 +0530

    add: mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 import Button from './components/button'
 
+const buttons = [
+  { bgColor: 'red', buttonText: 'Red' },
+  { bgColor: 'blue', buttonText: 'Blue' },
+  { bgColor: 'pink', buttonText: 'Pink' },
+  { bgColor: 'green', buttonText: 'Green' },
+  { bgColor: 'yellow', buttonText: 'Yellow' },
+  { bgColor: 'brown', buttonText: 'Brown' },
+  { bgColor: 'orange', buttonText: 'Orange' },
+]
+
 function App() {
   return (
     <>
-     <Button bgColor={'red'} buttonText={'Red'} />
-     <Button bgColor={'blue'} buttonText={'Blue'}/>
-     <Button bgColor={'pink'} buttonText={'Pink'}/>
-     <Button bgColor={'green'} buttonText={'Green'}/>
-     <Button bgColor={'yellow'} buttonText={'Yellow'}/>
-     <Button bgColor={'brown'} buttonText={'Brown'}/>
-
+      {buttons.map((el: any) => (
+        <Button bgColor={el.bgColor} buttonText={el.buttonText} />
+      ))}
     </>
   )
 }
